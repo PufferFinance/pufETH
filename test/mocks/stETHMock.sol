4d5b8110c9a5c44443f2c7c7d99a6db8a25f61eb
technovision99 <25871300+technovision99@users.noreply.github.com>
2024-03-06 10:19:42 -0800

    added 4626 properties test

--- test/mocks/stETHMock.sol
+++ test/mocks/stETHMock.sol
@@ -3,8 +3,9 @@ pragma solidity >=0.8.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "../../src/interface/Lido/IStETH.sol";
 
-contract stETHMock is ERC20, ERC20Burnable {
+contract stETHMock is IStETH,ERC20, ERC20Burnable {
     constructor() ERC20("Mock stETH", "mockStETH") { }
 
     uint256 public totalShares;
@@ -36,6 +37,18 @@ contract stETHMock is ERC20, ERC20Burnable {
         totalPooledEther = _totalPooledEther;
     }
 
+    function getTotalPooledEther() external view returns (uint256) {
+        return totalPooledEther;
+    }
+
+    function sharesOf(address _account) external view returns (uint256) {
+        //not implemented, just there for IstETH conformance
+    }
+
+    function transferShares(address _recipient, uint256 _sharesAmount) external returns (uint256) {
+        //not implemented, just there for IstETH conformance
+    }
+
     function getPooledEthByShares(uint256 _sharesAmount) public view returns (uint256) {
         if (totalShares == 0) {
             return 0;
@@ -48,5 +61,9 @@ contract stETHMock is ERC20, ERC20Burnable {
             return 0;
         }
         return _pooledEthAmount * totalShares / totalPooledEther;
+    }
+
+    function totalSupply() public view override(ERC20,IStETH) returns (uint256) {
+        return super.totalSupply();
     }
 }
