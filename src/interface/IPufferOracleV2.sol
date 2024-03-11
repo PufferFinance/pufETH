fee3d1891dc2df2f5c9e3b411f2922188d704ae1
Benjamin <benjaminxh+github@gmail.com>
2024-03-11 12:02:56 +0100

    update interface

--- src/interface/IPufferOracleV2.sol
+++ src/interface/IPufferOracleV2.sol
@@ -9,6 +9,7 @@ import { IPufferOracle } from "./IPufferOracle.sol";
  * @custom:security-contact security@puffer.fi
  */
 interface IPufferOracleV2 is IPufferOracle {
+    error InvalidUpdate();
     /**
      * @notice Emitted when the number of active Puffer validators is updated
      * @param numberOfActivePufferValidators is the number of active Puffer validators
@@ -16,18 +17,25 @@ interface IPufferOracleV2 is IPufferOracle {
     event NumberOfActiveValidators(uint256 numberOfActivePufferValidators);
 
     /**
+     * @notice Emitted when the total number of validators is updated
+     * @param oldNumberOfValidators is the old number of validators
+     * @param newNumberOfValidators is the new number of validators
+     */
+    event TotalNumberOfValidatorsUpdated(uint256 oldNumberOfValidators, uint256 newNumberOfValidators, uint256 epochNumber);
+
+    /**
      * @notice Returns the total number of active validators on Ethereum
      */
     function getTotalNumberOfValidators() external view returns (uint256);
 
     /**
-     * @notice Exits the validator, decreasing the `lockedETHAmount` by 32 ETH.
-     * It is called when when the Node Operator triggers the `retrieveBond` on the PufferProtocol.
+     * @notice Exits `validatorNumber` validators, decreasing the `lockedETHAmount` by validatorNumber * 32 ETH.
+     * It is called when when the validator exits the system in the `batchHandleWithdrawals` on the PufferProtocol.
      * In the same transaction, we are transferring full withdrawal ETH from the PufferModule to the Vault
      * Decrementing the `lockedETHAmount` by 32 ETH and we burn the Node Operator's pufETH (bond) if we need to cover up the loss.
      * @dev Restricted to PufferProtocol contract
      */
-    function exitValidator() external;
+    function exitValidators(uint256 validatorNumber) external;
 
     /**
      * @notice Increases the `lockedETHAmount` on the PufferOracle by 32 ETH to account for a new deposit.
