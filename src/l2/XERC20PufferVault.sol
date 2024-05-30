0efb224d36d891853cc01057ddfde1da87ce78b3
0xwalid <walid.wah@gmail.com>
2024-05-29 18:16:38 -0700

    rename comments

--- src/l2/XERC20PufferVault.sol
+++ src/l2/XERC20PufferVault.sol
@@ -37,242 +37,241 @@ contract XERC20PufferVault is IXERC20, AccessManagedUpgradeable, ERC20PermitUpgr
     /**
      * @notice Mints tokens for a user
      * @dev Can only be called by a bridge
-     * @param _user The address of the user who needs tokens minted
-     * @param _amount The amount of tokens being minted
+     * @param user The address of the user who needs tokens minted
+     * @param amount The amount of tokens being minted
      */
-    function mint(address _user, uint256 _amount) public {
-        _mintWithCaller(msg.sender, _user, _amount);
+    function mint(address user, uint256 amount) public {
+        _mintWithCaller(msg.sender, user, amount);
     }
 
     /**
      * @notice Burns tokens for a user
      * @dev Can only be called by a bridge
-     * @param _user The address of the user who needs tokens burned
-     * @param _amount The amount of tokens being burned
+     * @param user The address of the user who needs tokens burned
+     * @param amount The amount of tokens being burned
      */
-    function burn(address _user, uint256 _amount) public {
-        if (msg.sender != _user) {
-            _spendAllowance(_user, msg.sender, _amount);
+    function burn(address user, uint256 amount) public {
+        if (msg.sender != user) {
+            _spendAllowance(user, msg.sender, amount);
         }
 
-        _burnWithCaller(msg.sender, _user, _amount);
+        _burnWithCaller(msg.sender, user, amount);
     }
 
     /**
      * @notice Sets the lockbox address
      *
-     * @param _lockbox The address of the lockbox
+     * @param lockboxAddress The address of the lockbox
      */
-    function setLockbox(address _lockbox) public restricted {
-        // if (msg.sender != FACTORY) revert IXERC20_NotFactory();
-        lockbox = _lockbox;
+    function setLockbox(address lockboxAddress) public restricted {
+        lockbox = lockboxAddress;
 
-        emit LockboxSet(_lockbox);
+        emit LockboxSet(lockboxAddress);
     }
 
     /**
      * @notice Updates the limits of any bridge
      * @dev Can only be called by the owner
-     * @param _mintingLimit The updated minting limit we are setting to the bridge
-     * @param _burningLimit The updated burning limit we are setting to the bridge
-     * @param _bridge The address of the bridge we are setting the limits too
+     * @param mintingLimit The updated minting limit we are setting to the bridge
+     * @param burningLimit The updated burning limit we are setting to the bridge
+     * @param bridge The address of the bridge we are setting the limits too
      */
-    function setLimits(address _bridge, uint256 _mintingLimit, uint256 _burningLimit) external restricted {
-        if (_mintingLimit > (type(uint256).max / 2) || _burningLimit > (type(uint256).max / 2)) {
+    function setLimits(address bridge, uint256 mintingLimit, uint256 burningLimit) external restricted {
+        if (mintingLimit > (type(uint256).max / 2) || burningLimit > (type(uint256).max / 2)) {
             revert IXERC20_LimitsTooHigh();
         }
 
-        _changeMinterLimit(_bridge, _mintingLimit);
-        _changeBurnerLimit(_bridge, _burningLimit);
-        emit BridgeLimitsSet(_mintingLimit, _burningLimit, _bridge);
+        _changeMinterLimit(bridge, mintingLimit);
+        _changeBurnerLimit(bridge, burningLimit);
+        emit BridgeLimitsSet(mintingLimit, burningLimit, bridge);
     }
 
     /**
      * @notice Returns the max limit of a bridge
      *
-     * @param _bridge the bridge we are viewing the limits of
-     * @return _limit The limit the bridge has
+     * @param bridge the bridge we are viewing the limits of
+     * @return limit The limit the bridge has
      */
-    function mintingMaxLimitOf(address _bridge) public view returns (uint256 _limit) {
-        _limit = bridges[_bridge].minterParams.maxLimit;
+    function mintingMaxLimitOf(address bridge) public view returns (uint256 limit) {
+        limit = bridges[bridge].minterParams.maxLimit;
     }
 
     /**
      * @notice Returns the max limit of a bridge
      *
-     * @param _bridge the bridge we are viewing the limits of
-     * @return _limit The limit the bridge has
+     * @param bridge the bridge we are viewing the limits of
+     * @return limit The limit the bridge has
      */
-    function burningMaxLimitOf(address _bridge) public view returns (uint256 _limit) {
-        _limit = bridges[_bridge].burnerParams.maxLimit;
+    function burningMaxLimitOf(address bridge) public view returns (uint256 limit) {
+        limit = bridges[bridge].burnerParams.maxLimit;
     }
 
     /**
      * @notice Returns the current limit of a bridge
      *
-     * @param _bridge the bridge we are viewing the limits of
-     * @return _limit The limit the bridge has
+     * @param bridge the bridge we are viewing the limits of
+     * @return limit The limit the bridge has
      */
-    function mintingCurrentLimitOf(address _bridge) public view returns (uint256 _limit) {
-        _limit = _getCurrentLimit(
-            bridges[_bridge].minterParams.currentLimit,
-            bridges[_bridge].minterParams.maxLimit,
-            bridges[_bridge].minterParams.timestamp,
-            bridges[_bridge].minterParams.ratePerSecond
+    function mintingCurrentLimitOf(address bridge) public view returns (uint256 limit) {
+        limit = _getCurrentLimit(
+            bridges[bridge].minterParams.currentLimit,
+            bridges[bridge].minterParams.maxLimit,
+            bridges[bridge].minterParams.timestamp,
+            bridges[bridge].minterParams.ratePerSecond
         );
     }
 
     /**
      * @notice Returns the current limit of a bridge
      *
-     * @param _bridge the bridge we are viewing the limits of
-     * @return _limit The limit the bridge has
+     * @param bridge the bridge we are viewing the limits of
+     * @return limit The limit the bridge has
      */
-    function burningCurrentLimitOf(address _bridge) public view returns (uint256 _limit) {
-        _limit = _getCurrentLimit(
-            bridges[_bridge].burnerParams.currentLimit,
-            bridges[_bridge].burnerParams.maxLimit,
-            bridges[_bridge].burnerParams.timestamp,
-            bridges[_bridge].burnerParams.ratePerSecond
+    function burningCurrentLimitOf(address bridge) public view returns (uint256 limit) {
+        limit = _getCurrentLimit(
+            bridges[bridge].burnerParams.currentLimit,
+            bridges[bridge].burnerParams.maxLimit,
+            bridges[bridge].burnerParams.timestamp,
+            bridges[bridge].burnerParams.ratePerSecond
         );
     }
 
     /**
      * @notice Uses the limit of any bridge
-     * @param _bridge The address of the bridge who is being changed
-     * @param _change The change in the limit
+     * @param bridge The address of the bridge who is being changed
+     * @param change The change in the limit
      */
-    function _useMinterLimits(address _bridge, uint256 _change) internal {
-        uint256 _currentLimit = mintingCurrentLimitOf(_bridge);
-        bridges[_bridge].minterParams.timestamp = block.timestamp;
-        bridges[_bridge].minterParams.currentLimit = _currentLimit - _change;
+    function _useMinterLimits(address bridge, uint256 change) internal {
+        uint256 currentLimit = mintingCurrentLimitOf(bridge);
+        bridges[bridge].minterParams.timestamp = block.timestamp;
+        bridges[bridge].minterParams.currentLimit = currentLimit - change;
     }
 
     /**
      * @notice Uses the limit of any bridge
-     * @param _bridge The address of the bridge who is being changed
-     * @param _change The change in the limit
+     * @param bridge The address of the bridge who is being changed
+     * @param change The change in the limit
      */
-    function _useBurnerLimits(address _bridge, uint256 _change) internal {
-        uint256 _currentLimit = burningCurrentLimitOf(_bridge);
-        bridges[_bridge].burnerParams.timestamp = block.timestamp;
-        bridges[_bridge].burnerParams.currentLimit = _currentLimit - _change;
+    function _useBurnerLimits(address bridge, uint256 change) internal {
+        uint256 currentLimit = burningCurrentLimitOf(bridge);
+        bridges[bridge].burnerParams.timestamp = block.timestamp;
+        bridges[bridge].burnerParams.currentLimit = currentLimit - change;
     }
 
     /**
      * @notice Updates the limit of any bridge
      * @dev Can only be called by the owner
-     * @param _bridge The address of the bridge we are setting the limit too
-     * @param _limit The updated limit we are setting to the bridge
+     * @param bridge The address of the bridge we are setting the limit too
+     * @param limit The updated limit we are setting to the bridge
      */
-    function _changeMinterLimit(address _bridge, uint256 _limit) internal {
-        uint256 _oldLimit = bridges[_bridge].minterParams.maxLimit;
-        uint256 _currentLimit = mintingCurrentLimitOf(_bridge);
-        bridges[_bridge].minterParams.maxLimit = _limit;
+    function _changeMinterLimit(address bridge, uint256 limit) internal {
+        uint256 oldLimit = bridges[bridge].minterParams.maxLimit;
+        uint256 currentLimit = mintingCurrentLimitOf(bridge);
+        bridges[bridge].minterParams.maxLimit = limit;
 
-        bridges[_bridge].minterParams.currentLimit = _calculateNewCurrentLimit(_limit, _oldLimit, _currentLimit);
+        bridges[bridge].minterParams.currentLimit = _calculateNewCurrentLimit(limit, oldLimit, currentLimit);
 
-        bridges[_bridge].minterParams.ratePerSecond = _limit / _DURATION;
-        bridges[_bridge].minterParams.timestamp = block.timestamp;
+        bridges[bridge].minterParams.ratePerSecond = limit / _DURATION;
+        bridges[bridge].minterParams.timestamp = block.timestamp;
     }
 
     /**
      * @notice Updates the limit of any bridge
      * @dev Can only be called by the owner
-     * @param _bridge The address of the bridge we are setting the limit too
-     * @param _limit The updated limit we are setting to the bridge
+     * @param bridge The address of the bridge we are setting the limit too
+     * @param limit The updated limit we are setting to the bridge
      */
-    function _changeBurnerLimit(address _bridge, uint256 _limit) internal {
-        uint256 _oldLimit = bridges[_bridge].burnerParams.maxLimit;
-        uint256 _currentLimit = burningCurrentLimitOf(_bridge);
-        bridges[_bridge].burnerParams.maxLimit = _limit;
+    function _changeBurnerLimit(address bridge, uint256 limit) internal {
+        uint256 oldLimit = bridges[bridge].burnerParams.maxLimit;
+        uint256 currentLimit = burningCurrentLimitOf(bridge);
+        bridges[bridge].burnerParams.maxLimit = limit;
 
-        bridges[_bridge].burnerParams.currentLimit = _calculateNewCurrentLimit(_limit, _oldLimit, _currentLimit);
+        bridges[bridge].burnerParams.currentLimit = _calculateNewCurrentLimit(limit, oldLimit, currentLimit);
 
-        bridges[_bridge].burnerParams.ratePerSecond = _limit / _DURATION;
-        bridges[_bridge].burnerParams.timestamp = block.timestamp;
+        bridges[bridge].burnerParams.ratePerSecond = limit / _DURATION;
+        bridges[bridge].burnerParams.timestamp = block.timestamp;
     }
 
     /**
      * @notice Updates the current limit
      *
-     * @param _limit The new limit
-     * @param _oldLimit The old limit
-     * @param _currentLimit The current limit
+     * @param limit The new limit
+     * @param oldLimit The old limit
+     * @param currentLimit The current limit
      * @return _newCurrentLimit The new current limit
      */
-    function _calculateNewCurrentLimit(uint256 _limit, uint256 _oldLimit, uint256 _currentLimit)
+    function _calculateNewCurrentLimit(uint256 limit, uint256 oldLimit, uint256 currentLimit)
         internal
         pure
         returns (uint256 _newCurrentLimit)
     {
         uint256 _difference;
 
-        if (_oldLimit > _limit) {
-            _difference = _oldLimit - _limit;
-            _newCurrentLimit = _currentLimit > _difference ? _currentLimit - _difference : 0;
+        if (oldLimit > limit) {
+            _difference = oldLimit - limit;
+            _newCurrentLimit = currentLimit > _difference ? currentLimit - _difference : 0;
         } else {
-            _difference = _limit - _oldLimit;
-            _newCurrentLimit = _currentLimit + _difference;
+            _difference = limit - oldLimit;
+            _newCurrentLimit = currentLimit + _difference;
         }
     }
 
     /**
      * @notice Gets the current limit
      *
-     * @param _currentLimit The current limit
-     * @param _maxLimit The max limit
-     * @param _timestamp The timestamp of the last update
-     * @param _ratePerSecond The rate per second
-     * @return _limit The current limit
+     * @param currentLimit The current limit
+     * @param maxLimit The max limit
+     * @param timestamp The timestamp of the last update
+     * @param ratePerSecond The rate per second
+     * @return limit The current limit
      */
-    function _getCurrentLimit(uint256 _currentLimit, uint256 _maxLimit, uint256 _timestamp, uint256 _ratePerSecond)
+    function _getCurrentLimit(uint256 currentLimit, uint256 maxLimit, uint256 timestamp, uint256 ratePerSecond)
         internal
         view
-        returns (uint256 _limit)
+        returns (uint256 limit)
     {
-        _limit = _currentLimit;
-        if (_limit == _maxLimit) {
-            return _limit;
-        } else if (_timestamp + _DURATION <= block.timestamp) {
-            _limit = _maxLimit;
-        } else if (_timestamp + _DURATION > block.timestamp) {
-            uint256 _timePassed = block.timestamp - _timestamp;
-            uint256 _calculatedLimit = _limit + (_timePassed * _ratePerSecond);
-            _limit = _calculatedLimit > _maxLimit ? _maxLimit : _calculatedLimit;
+        limit = currentLimit;
+        if (limit == maxLimit) {
+            return limit;
+        } else if (timestamp + _DURATION <= block.timestamp) {
+            limit = maxLimit;
+        } else if (timestamp + _DURATION > block.timestamp) {
+            uint256 _timePassed = block.timestamp - timestamp;
+            uint256 _calculatedLimit = limit + (_timePassed * ratePerSecond);
+            limit = _calculatedLimit > maxLimit ? maxLimit : _calculatedLimit;
         }
     }
 
     /**
      * @notice Internal function for burning tokens
      *
-     * @param _caller The caller address
-     * @param _user The user address
-     * @param _amount The amount to burn
+     * @param caller The caller address
+     * @param user The user address
+     * @param amount The amount to burn
      */
-    function _burnWithCaller(address _caller, address _user, uint256 _amount) internal {
-        if (_caller != lockbox) {
-            uint256 _currentLimit = burningCurrentLimitOf(_caller);
-            if (_currentLimit < _amount) revert IXERC20_NotHighEnoughLimits();
-            _useBurnerLimits(_caller, _amount);
+    function _burnWithCaller(address caller, address user, uint256 amount) internal {
+        if (caller != lockbox) {
+            uint256 currentLimit = burningCurrentLimitOf(caller);
+            if (currentLimit < amount) revert IXERC20_NotHighEnoughLimits();
+            _useBurnerLimits(caller, amount);
         }
-        _burn(_user, _amount);
+        _burn(user, amount);
     }
 
     /**
      * @notice Internal function for minting tokens
      *
-     * @param _caller The caller address
-     * @param _user The user address
-     * @param _amount The amount to mint
+     * @param caller The caller address
+     * @param user The user address
+     * @param amount The amount to mint
      */
-    function _mintWithCaller(address _caller, address _user, uint256 _amount) internal {
-        if (_caller != lockbox) {
-            uint256 _currentLimit = mintingCurrentLimitOf(_caller);
-            if (_currentLimit < _amount) revert IXERC20_NotHighEnoughLimits();
-            _useMinterLimits(_caller, _amount);
+    function _mintWithCaller(address caller, address user, uint256 amount) internal {
+        if (caller != lockbox) {
+            uint256 currentLimit = mintingCurrentLimitOf(caller);
+            if (currentLimit < amount) revert IXERC20_NotHighEnoughLimits();
+            _useMinterLimits(caller, amount);
         }
-        _mint(_user, _amount);
+        _mint(user, amount);
     }
 
     /**
